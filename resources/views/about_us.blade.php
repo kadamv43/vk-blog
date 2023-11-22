b1d95ed73975004dc8d60e15e65a98831953e33e
Vinayak Kadam <kadamv43@gmail.com>
2023-11-22 18:52:46 +0530

    blog added

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -17,18 +17,13 @@
                 <img src="{{ asset('assets/frontend/images/img_h_2.jpg') }}" alt="image" class="img-fluid rounded">
             </div>
             <div class="col-lg-6 pl-lg-5">
-                <h3 class="mb-4 section-title">History</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
-                    the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
-                    language ocean.</p>
-                <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a
-                    paradisematic country, in which roasted parts of sentences fly into your mouth. </p>
-                <ul class="list-check list-unstyled primary">
-                    <li>Far far away, behind the word mountains</li>
-                    <li>Vokalia and Consonantia there</li>
-                    <li>Separated they live</li>
-                    <li>Semantics a large language ocean</li>
-                </ul>
+                <p>Hello readers,
+                    we are vkblog team, starting this blog to ensure readers will get updated and correct information through this blog.
+                    hope you will give your love to this blog and appreciate our hardwork
+                    please feel free to share feedback or suggestions to our team we are happy to hear from you.
+                    please like, share and subscribe to our blog to get latest updates
+                    
+                    Thank You.</p>
             </div>
         </div>
     </div>
