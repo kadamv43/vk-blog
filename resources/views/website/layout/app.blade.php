838d57bcd51584dbfeb24b7554b56dcfb85d79d6
Vinayak Kadam <kadamv43@gmail.com>
2026-07-08 18:34:54 +0530

    robot txt issue resolved

--- resources/views/website/layout/app.blade.php
+++ resources/views/website/layout/app.blade.php
@@ -19,7 +19,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="@yield('meta_description', 'VKBlog')">
-    <meta name="robots" content="noindex, Nofollow, Noimageindex">
 
     <title>@yield('title', 'VKBlog')</title>
 
